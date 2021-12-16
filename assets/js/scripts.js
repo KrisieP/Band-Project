729e98d3f40232f333219bc90001b8679789e8fb
KrisieP <krisie.p31@gmail.com>
2021-12-16 22:02:01 +0000

    added script to close both modals upon closing subscription success index.htm & scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,5 @@
 //The custom Jquery below was found here URL(https://github.com/AnoukSmet/Naturazy/blob/master/index.html)
 
-
 $('#contact-form').submit(function (e) {
     e.preventDefault();
     $('#submitModal').modal('show');
@@ -11,6 +10,12 @@ $('#modal-form').submit(function (e) {
     e.preventDefault();
     $('#submitsubscription').modal('show');
     $('#modal-form')[0].reset();
+});
+
+$('.close-submit-modal').click(function (e) {
+    e.preventDefault();
+    $('#submitsubscription').modal('hide');
+    $('#subscriptionmodalcenter').modal('hide');
 });
 
 //The custom Jquery below was found here URL(https://mdbootstrap.com/support/general/auto-close-navbar-when-click-on-link-responsive-mode/)-->
