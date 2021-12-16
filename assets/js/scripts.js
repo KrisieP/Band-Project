6fee24c5a7b0e02adc9599b49de8a1b5e603a7f4
KrisieP <krisie.p31@gmail.com>
2021-12-16 21:06:06 +0000

    js folder created, scripts.js created, moved customer js from index.html to scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+//The custom Jquery below was found here URL(https://github.com/AnoukSmet/Naturazy/blob/master/index.html)
+
+
+$('#contact-form').submit(function (e) {
+    e.preventDefault();
+    $('#submitModal').modal('show');
+    $('#contact-form')[0].reset();
+});
+
+$('#modal-form').submit(function (e) {
+    e.preventDefault();
+    $('#submitsubscription').modal('show');
+    $('#modal-form')[0].reset();
+});
+
+//The custom Jquery below was found here URL(https://mdbootstrap.com/support/general/auto-close-navbar-when-click-on-link-responsive-mode/)-->
+
+$('.navbar-collapse a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+});
